9174317fc33121be6bdae3f64e46dde16d62b7b0
abegizaw3 <135074156+abegizaw3@users.noreply.github.com>
2023-06-15 10:08:04 -0700

    static files

--- server.ts
+++ server.ts
@@ -5,20 +5,20 @@ const app = express(); //sets up the actual server
 
 app.set("view engine", "ejs"); //sets up view engine
 
-app.use(logger); //use the middleware define below. Will be used everywhere since you defined it at the top
+app.use(logger); //Middleware Will be used everywhere since you defined it at the top
+app.use(express.static("public")); //run all static pages in public
 
 /**
  * @date 6/15/2023 - 8:01:59 AM
- * When someone makes a get request to this route, it sends Hi
- * Url would be localhost/.  Parameters are (path, function). Also passed middleware logger
+ * When someone makes a get request to this route, it renders/sends whatever . Also passed middleware logger
  */
 app.get("/", logger, (req: Request, res: Response) => {
   console.log("object");
   res.render("index", { text1243: "World" }); //passing info to our ejs file
   // res.download('server.ts') download a file
   // res.status(500).json({ message: "Error"}) //Internal server error (code 500) and json code with it
-  // res.status(500).send("Hi") //Internal server error (code 500) and unqie message with it
-  // res.send("Hi") //very generic. Not used very often but good for testing purposes
+  // res.status(500).send("Hi") //Internal server error (code 500) and message with it
+  // res.send("Hi") Good for testing purposes
 });
 
 app.use("/users", userRouter);
