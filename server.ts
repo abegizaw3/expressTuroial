0d11e51dda10fc52ff9c635daa9c535d29727c89
abegizaw3 <135074156+abegizaw3@users.noreply.github.com>
2023-06-15 08:12:43 -0700

    basic git requests

---
+++ server.ts
@@ -0,0 +1,21 @@
+import express, { Request, Response } from 'express';
+
+const app = express() //sets up the actual server
+
+
+/**
+ * @date 6/15/2023 - 8:01:59 AM
+ * When someone makes a get request to this route, it sends Hi
+ *
+ * Url would be localhost/.  Parameters are (path, function)
+ */
+app.get('/', (req: Request, res: Response) => {
+    console.log("object");
+    res.render('index')
+    // res.download('server.ts') download a file
+    // res.status(500).json({ message: "Error"}) //Internal server error (code 500) and json code with it
+    // res.status(500).send("Hi") //Internal server error (code 500) and unqie message with it
+    // res.send("Hi") //very generic. Not used very often but good for testing purposes
+})
+
+app.listen(3000) //app listens on port 3000 for a bunch of api requests.
